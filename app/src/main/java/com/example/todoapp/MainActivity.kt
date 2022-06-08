14b07e5ab01c5817eacc3deed4d3fd7b22765704
BrayanCordero <corderobrayan@yaho.com>
2022-06-08 17:30:20 -0400

    added event adapter, and did main fragment logic.

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Need to create private val to use binding
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root) // change this to binding.root
+
+        val navController = findNavController(R.id.nav_container_fragment) //id for fragment in activity_main.xml
+        binding.navigationBar.setupWithNavController(navController) // bar in activity_main.xml
     }
 }
+
+
+//first need to add all the files needed for the app
+//UI,Models,Adapter,Menu,Navigation
+// navigation allows you to move from one fragment to another
+//Then do activity_main.xml and come back and add navController
+//Do menu and add it to activity_main.xml inside the BottomNavigationView
+//Do fragment_main.xml , then fragment_entry.xml , then fragment_details.xml
+//Add a new layout for the events list todoItems
+//Next the logic for fragments; MainFragment, EntryFragment, and DetailsFragment
+//and the logic for ClickHandler and EventAdapter
--- app/src/main/java/com/example/todoapp/adapter/EventAdapter.kt
+++ app/src/main/java/com/example/todoapp/adapter/EventAdapter.kt
@@ -1,4 +1,65 @@
 package com.example.todoapp.adapter
 
-class EventAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.EventTodoBinding
+import com.example.todoapp.models.Event
+
+
+class EventAdapter(
+
+    // click handling with interface
+    private val onEventClickHandler: ClickHandler,
+    private val eventList: MutableList<Event> = mutableListOf(),
+
+    // click handling with high order function
+    private val onClickEventHighOrderFunction: (Event) -> Unit
+
+) : RecyclerView.Adapter<EventViewHolder>() //logic for the Recycler view
+{
+    //function that takes in an Event and updates the view when a new event is added
+    fun updateEventList(event: Event){
+        eventList.add(event)
+        notifyItemInserted(eventList.indexOf(event))
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder =
+        EventViewHolder(
+            //ToDoItemBinding from event_todo.xml
+            EventTodoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+        override fun onBindViewHolder(holder: EventViewHolder, position: Int) =
+            holder.bind(eventList[position], onEventClickHandler, onClickEventHighOrderFunction)
+
+        override fun getItemCount(): Int = eventList.size
+    }
+
+
+//this class is getting user input and binding it to the model. two ways to handle the click
+class EventViewHolder(
+    private val binding: EventTodoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(
+        event: Event, onEventClickHandler: ClickHandler, onClickEventHighOrderFunction: (Event) -> Unit
+    ) {
+        binding.eventName.text = event.name
+        binding.eventCategory.text = event.category
+        binding.eventDate.text = event.date
+
+        itemView.setOnClickListener {
+            // this is handling the click with interface
+            // onEventClickHandler.onEventItemClick(event)
+
+            // this is handling click with high order function
+            onClickEventHighOrderFunction(event)
+        }
+    }
 }
