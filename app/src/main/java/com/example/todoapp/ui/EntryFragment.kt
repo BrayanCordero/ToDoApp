0e6b870ee52fc7b7e3774add2f42dbe3123d96e7
BrayanCordero <corderobrayan@yaho.com>
2022-06-08 22:32:40 -0400

    fix entry view

--- app/src/main/java/com/example/todoapp/ui/EntryFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/EntryFragment.kt
@@ -3,11 +3,11 @@ package com.example.todoapp.ui
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentEntryBinding
